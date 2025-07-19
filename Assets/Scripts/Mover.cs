0fad8a5f2524401acb8a58af6581af91fa7e0833
Jasper Andrew L. Chan <144504479+Jasper-Andrew-L-Chan@users.noreply.github.com>
2025-07-19 18:58:34 +0800

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("WELCOME TO THE GAME.");
+        Debug.Log("Use WASD move, Space to move up.");
+    }
+    
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = Input.GetAxis("Jump") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
