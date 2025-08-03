66c8ac8a4e598b60c61053509e55c45df65730c7
Jasper Andrew L. Chan <144504479+Jasper-Andrew-L-Chan@users.noreply.github.com>
2025-08-03 16:12:42 +0800

    Added Destroy Obj, Trigger Projectile, add Hazards To Level

--- Assets/Scripts/FlyatPlayer.cs
+++ Assets/Scripts/FlyatPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FlyatPlayer : MonoBehaviour
@@ -6,15 +7,36 @@ public class FlyatPlayer : MonoBehaviour
     [SerializeField] float Speed = 1.0f;
     Vector3 playerPosition;
 
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
     void Start()
     {
         playerPosition = player.transform.position;
     }
 
-    
+
 
     void Update()
     {
+        MoveToPlayer();
+        DestroyWhenReached();
+
+    }
+
+    void MoveToPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, Speed * Time.deltaTime);
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
+
+        
     }
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-
+    [SerializeField] float jumpStrength = 10f;
     void Start()
     {
         PrintInstructions();
@@ -24,7 +24,7 @@ public class Mover : MonoBehaviour
     void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = Input.GetAxis("Jump") * Time.deltaTime * moveSpeed;
+        float yValue = Input.GetAxis("Jump") * Time.deltaTime * jumpStrength;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, yValue, zValue);
